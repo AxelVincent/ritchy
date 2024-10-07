e306b5a12d3999fadd3f60d5284fb9ccf2aa1967
Axel Vincent <vincentaxel@gmail.com>
2024-10-07 10:36:41 +0200

    Add constraint on lead_email

---
+++ drizzle/0005_flimsy_dorian_gray.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "lead_email" ADD CONSTRAINT "lead_email_lead_id_email_unique" UNIQUE("lead_id","email");
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,42 +6,49 @@ import {
 	serial,
 	text,
 	timestamp,
-} from "drizzle-orm/pg-core";
+	unique
+} from 'drizzle-orm/pg-core'
 
-export const queryParam = pgTable("query_param", {
-	id: serial("id").primaryKey(),
-	researchQuery: text("research_query").notNull(),
-	latitude: doublePrecision("latitude").notNull(),
-	longitude: doublePrecision("longitude").notNull(),
-	zoom: text("zoom").notNull(),
-	createdAt: timestamp("created_at").notNull().defaultNow(),
-	updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
+export const queryParam = pgTable('query_param', {
+	id: serial('id').primaryKey(),
+	researchQuery: text('research_query').notNull(),
+	latitude: doublePrecision('latitude').notNull(),
+	longitude: doublePrecision('longitude').notNull(),
+	zoom: text('zoom').notNull(),
+	createdAt: timestamp('created_at').notNull().defaultNow(),
+	updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
 
-export const lead = pgTable("lead", {
-	id: serial("id").primaryKey(),
-	queryParamId: integer("query_param_id")
+export const lead = pgTable('lead', {
+	id: serial('id').primaryKey(),
+	queryParamId: integer('query_param_id')
 		.notNull()
 		.references(() => queryParam.id),
-	name: text("name").notNull(),
-	address: text("address").notNull(),
-	phone: text("phone"),
-	website: text("website"),
-	latitude: doublePrecision("latitude").notNull(),
-	longitude: doublePrecision("longitude").notNull(),
-	description: text("description"),
-	websiteContent: text("website_content"),
-	createdAt: timestamp("created_at").notNull().defaultNow(),
-	updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
+	name: text('name').notNull(),
+	address: text('address').notNull(),
+	phone: text('phone'),
+	website: text('website'),
+	latitude: doublePrecision('latitude').notNull(),
+	longitude: doublePrecision('longitude').notNull(),
+	description: text('description'),
+	websiteContent: text('website_content'),
+	createdAt: timestamp('created_at').notNull().defaultNow(),
+	updatedAt: timestamp('updated_at').notNull().defaultNow()
+})
 
-export const leadEmail = pgTable("lead_email", {
-	id: serial("id").primaryKey(),
-	leadId: integer("lead_id")
-		.notNull()
-		.references(() => lead.id),
-	email: text("email").notNull(),
-	isMatchingDomain: boolean("is_matching_domain").notNull().default(false),
-	createdAt: timestamp("created_at").notNull().defaultNow(),
-	updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
+export const leadEmail = pgTable(
+	'lead_email',
+	{
+		id: serial('id').primaryKey(),
+		leadId: integer('lead_id')
+			.notNull()
+			.references(() => lead.id),
+		email: text('email').notNull(),
+		isMatchingDomain: boolean('is_matching_domain').notNull().default(false),
+		createdAt: timestamp('created_at').notNull().defaultNow(),
+		updatedAt: timestamp('updated_at').notNull().defaultNow()
+	},
+	(table) => ({
+		uniqueLeadIdEmail: unique().on(table.leadId, table.email)
+	})
+)
