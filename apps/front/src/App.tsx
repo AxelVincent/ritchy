92044bc289c9aabdd49032214d3640bcd2431d7c
Axel Vincent <vincentaxel@gmail.com>
2024-11-14 20:04:50 +0000

    [front] Install global shadcn theme

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from './components/theme-provider'
 // import EnhancedSalesMapTool from './components/enhanced-sales-map-tool-with-gmap-and-places'
 import { MapDisplay } from './features/MapDisplay/MapDisplay'
 
@@ -8,9 +9,11 @@ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MapDisplay />
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <MapDisplay />
+      </QueryClientProvider>
+    </ThemeProvider>
   )
 }
 
