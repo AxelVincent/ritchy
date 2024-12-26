ac754f134f4136a13972df826149c139318f5eec
Axel Vincent <vincentaxel@gmail.com>
2024-12-26 12:53:00 +0100

    [front] Replace tanstack helper

--- apps/front/src/routes/__root.tsx
+++ apps/front/src/routes/__root.tsx
@@ -5,7 +5,7 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools position="top-right" initialIsOpen={false} />
+      <TanStackRouterDevtools position="bottom-right" initialIsOpen={false} />
     </>
   ),
 })
