16e8b196a5025e59a900d48b8e201da99c3f401c
Axel Vincent <vincentaxel@gmail.com>
2025-01-07 15:44:06 +0000

    [front] Typo

--- apps/front/src/features/MapDisplay/components/data_table/DataTable.tsx
+++ apps/front/src/features/MapDisplay/components/data_table/DataTable.tsx
@@ -203,7 +203,7 @@ export const DataTable = <TData extends SearchResult, TValue>({
       </div>
       <div className="flex-1 overflow-scroll min-h-0 min-w-0 border">
         <div className="w-[100px] h-[100px]">
-          <table className="border-separate border-spacing-0 min-w-full border-spacing-0">
+          <table className="border-separate border-spacing-0 min-w-full">
             <thead>
               {table.getHeaderGroups().map((headerGroup) => (
                 <tr key={headerGroup.id} className="hover:bg-transparent">
