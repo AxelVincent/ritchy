d76b79d214123de3c5698a26485faa062dc56d2c
Axel Vincent <vincentaxel@gmail.com>
2025-02-18 18:21:34 +0000

    [front] Allow wignup before showing a only desktop message

--- apps/front/src/routes/_auth.tsx
+++ apps/front/src/routes/_auth.tsx
@@ -9,6 +9,8 @@ export const Route = createFileRoute('/_auth')({
 })
 
 function AuthedLayout() {
+  const isMobile = window.innerWidth < 768
+
   useEffect(() => {
     // Initialize Sleekplan
     window.$sleek = []
@@ -29,31 +31,36 @@ function AuthedLayout() {
     }
   }, [])
 
+  if (isMobile) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-background p-4">
+        <div className="max-w-md text-center">
+          <h1 className="text-2xl font-bold mb-4 text-foreground">
+            Mobile not supported
+          </h1>
+          <p className="text-muted-foreground">
+            This application is optimized for desktop and tablet use. Please
+            access it from a larger screen device.
+            <br />
+            If you really need to use it on a mobile device, please make a
+            feature request through the feedback button or this contact form
+            <a href="https://www.ritchy.io/contact" className="underline">
+              https://www.ritchy.io/contact
+            </a>
+            .
+          </p>
+          <p className="mt-4 italic">Your friends at Ritchy 🐕</p>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <SignedIn>
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset className="h-full w-full overflow-hidden">
-            {/* <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-              <div className="flex items-center gap-2 px-4">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
-                <Breadcrumb>
-                  <BreadcrumbList>
-                    <BreadcrumbItem className="hidden md:block">
-                      <BreadcrumbLink href="#">
-                        Building Your Application
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator className="hidden md:block" />
-                    <BreadcrumbItem>
-                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                    </BreadcrumbItem>
-                  </BreadcrumbList>
-                </Breadcrumb>
-              </div>
-            </header> */}
             <Outlet />
           </SidebarInset>
         </SidebarProvider>
