1ff37334475dbbe1d8d57925008d117f72b945eb
Axel Vincent <vincentaxel@gmail.com>
2025-02-19 13:21:38 +0000

    [back] Migration script error fix

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -34,6 +34,7 @@ export const list = pgTable('list', {
   userId: uuid('user_id')
     .notNull()
     .references(() => user.id),
+  userIdNew: uuid('user_id_new'),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
@@ -63,6 +64,7 @@ export const note = pgTable('note', {
   userId: uuid('user_id')
     .notNull()
     .references(() => user.id),
+  userIdNew: uuid('user_id_new'),
   note: text('note').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
@@ -80,6 +82,7 @@ export const search = pgTable('search', {
   userId: uuid('user_id')
     .notNull()
     .references(() => user.id),
+  userIdNew: uuid('user_id_new'),
   latitude: numeric('latitude').notNull(),
   longitude: numeric('longitude').notNull(),
   radiusInMeters: integer('radius_in_meters').notNull(),
