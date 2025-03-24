e267f81bd2a9545ef3cae4dcc088460e93dc7843
Axel Vincent <vincentaxel@gmail.com>
2025-03-24 17:09:12 +0000

    [core] Fix drizzle error (3)

--- apps/api/drizzle/0026_goofy_brood.sql
+++ apps/api/drizzle/0026_goofy_brood.sql
@@ -16,6 +16,8 @@ ALTER TABLE "list" ALTER COLUMN "user_id" TYPE uuid USING user_id::uuid;
 --> statement-breakpoint
 ALTER TABLE "note" ALTER COLUMN "user_id" TYPE uuid USING user_id::uuid;
 --> statement-breakpoint
+ALTER TABLE "search" ALTER COLUMN "user_id" TYPE uuid USING user_id::uuid;
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "enrichment" ADD CONSTRAINT "enrichment_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
