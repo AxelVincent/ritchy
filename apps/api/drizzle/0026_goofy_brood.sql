e3ee68c1a2a858272d6e0fedb6b84007f60e40df
Axel Vincent <vincentaxel@gmail.com>
2025-03-24 17:05:15 +0000

    [core] Fix drizzle error

--- apps/api/drizzle/0026_goofy_brood.sql
+++ apps/api/drizzle/0026_goofy_brood.sql
@@ -12,6 +12,8 @@ ALTER TABLE "status" DROP CONSTRAINT IF EXISTS "status_user_id_user_id_fk";
 --> statement-breakpoint
 ALTER TABLE "subscription" DROP CONSTRAINT IF EXISTS "subscription_user_id_user_id_fk";
 --> statement-breakpoint
+ALTER TABLE "list" ALTER COLUMN "user_id" TYPE uuid USING user_id::uuid;
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "enrichment" ADD CONSTRAINT "enrichment_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
