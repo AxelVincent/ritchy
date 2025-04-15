8297b67d1fadd23e2340ac17e538cc2bc7ef5b44
Axel Vincent <vincentaxel@gmail.com>
2025-04-15 15:26:24 +0100

    [front] Set back default police size

--- apps/front/tailwind.config.ts
+++ apps/front/tailwind.config.ts
@@ -111,7 +111,11 @@ const config = {
       },
     },
   },
-  plugins: [animate, require('tailwindcss-animate'), addVariablesForColors],
+  plugins: [
+    animate,
+    require('tailwindcss-animate'),
+    addVariablesForColors,
+  ],
 } satisfies Config
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
