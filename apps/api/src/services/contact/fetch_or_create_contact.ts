b4c8f4dea5db77ee39afdbbb106510f8c7b815da
Axel Vincent <32762860+AxelVincent@users.noreply.github.com>
2025-06-25 17:50:23 +0100

    [RIT-41] save enrichment data service (#272)
    * Revert "Revert "[RIT-41] save enrichment data service" (#269)"
    
    This reverts commit 212ce62a974e1bdf972c26f5bd33009cac0c01b2.
    
    * [RIT-41] Use transaction for read
    
    * [RIT-41] Self review

---
+++ apps/api/src/services/contact/fetch_or_create_contact.ts
@@ -0,0 +1,40 @@
+import { and, eq } from 'drizzle-orm'
+import { db } from '../../db/db'
+import { contact } from '../../db/schema'
+import { getPlaceDetailsV1 } from '../../external/google_maps/place_details_V1'
+
+export const fetchOrCreateContact = async (placeId: string, userId: string) => {
+  const [existingContact] = await db
+    .select()
+    .from(contact)
+    .where(and(eq(contact.placeId, placeId), eq(contact.userId, userId)))
+
+  if (!existingContact) {
+    const place = await getPlaceDetailsV1(placeId)
+    const [newContact] = await db
+      .insert(contact)
+      .values({
+        placeId,
+        userId,
+        firstname: place.name,
+        lastname: '',
+        email: '',
+        phone: place.phone,
+      })
+      .returning()
+
+    return newContact
+  }
+
+  return existingContact
+}
+
+export const fetchOrCreateContacts = async (
+  placeIds: string[],
+  userId: string,
+) =>
+  Promise.all(
+    placeIds.map(
+      async (placeId) => await fetchOrCreateContact(placeId, userId),
+    ),
+  )
