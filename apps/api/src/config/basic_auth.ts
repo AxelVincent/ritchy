e17a824a9c3e31088cd2044bb857d97971ea60fe
Axel Vincent <vincentaxel@gmail.com>
2025-07-30 12:00:22 +0100

    [API] Linter

--- apps/api/src/config/basic_auth.ts
+++ apps/api/src/config/basic_auth.ts
@@ -2,12 +2,12 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   QUEUE_DASH_USERNAME: z.string().min(1),
-  QUEUE_DASH_PASSWORD: z.string().min(1)
+  QUEUE_DASH_PASSWORD: z.string().min(1),
 })
 
 const BASIC_AUTH = envSchema.parse(process.env)
 
 export const BASIC_AUTH_CONFIG = {
   username: BASIC_AUTH.QUEUE_DASH_USERNAME,
-  password: BASIC_AUTH.QUEUE_DASH_PASSWORD
+  password: BASIC_AUTH.QUEUE_DASH_PASSWORD,
 } as const
