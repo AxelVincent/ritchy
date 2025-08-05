43db8119ed3f9944f29a843ccc2b19e5e2eb44a4
Axel Vincent <vincentaxel@gmail.com>
2025-08-05 10:35:50 +0100

    [NOTICKET] Put back original resizable size

--- apps/front/src/components/ui/resizable.tsx
+++ apps/front/src/components/ui/resizable.tsx
@@ -34,7 +34,7 @@ const ResizableHandle = ({
   >
     {withHandle && (
       <div className="z-20 flex h-8 w-8 items-center justify-center rounded-sm border bg-border hover:bg-gray-300 transition-all duration-200 hover:w-12">
-        <DragHandleDots2Icon className="h-5 w-5" />
+        <DragHandleDots2Icon className="h-4 w-4" />
       </div>
     )}
   </ResizablePrimitive.PanelResizeHandle>
