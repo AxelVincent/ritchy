bf6d14cf66292988830ed52702e5f7930d81a821
Axel Vincent <vincentaxel@gmail.com>
2025-08-21 17:53:51 +0100

    [NOTICKET] Sanitize brightdata log

--- apps/api/src/external/brightdata/index.ts
+++ apps/api/src/external/brightdata/index.ts
@@ -25,7 +25,6 @@ export const scrapeWithBrightdata = async (
         metadata: {
           url,
           error: result.error,
-          rawResult: result,
           responseTimeInSeconds: (Date.now() - time) / 1000,
         },
       })
