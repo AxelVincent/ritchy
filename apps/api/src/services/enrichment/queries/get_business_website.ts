a0b5a9a01e1fa934afd3bac3b079925a9252eeb9
Axel Vincent <32762860+AxelVincent@users.noreply.github.com>
2025-09-02 17:05:02 +0100

    [NOTICKET] Fix enrichment (#335)

--- apps/api/src/services/enrichment/queries/get_business_website.ts
+++ apps/api/src/services/enrichment/queries/get_business_website.ts
@@ -1,9 +1,6 @@
 import { eq } from 'drizzle-orm'
 import { db } from '../../../db/db'
 import { place as placeTable, userPlace } from '../../../db/schema'
-import type { PreferredPlace } from '../../../external/google_maps/types'
-import { REDIS_KEYS } from '../../../internal/redis/keys'
-import { redisClient } from '../../../internal/redis/redis'
 
 export const getBusinessWebsite = async (userPlaceId: string) => {
   const [place] = await db
@@ -13,15 +10,5 @@ export const getBusinessWebsite = async (userPlaceId: string) => {
     .where(eq(userPlace.id, userPlaceId))
     .limit(1)
 
-  if (!place) {
-    return null
-  }
-  const key = REDIS_KEYS.place(place.place.source_id)
-  const sourcePlace = await redisClient.get<PreferredPlace>(key)
-
-  if (!sourcePlace) {
-    return null
-  }
-
-  return sourcePlace.data.websiteUri
+  return place.place.website
 }
